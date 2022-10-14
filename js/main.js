382461c507c61898a029173e49edd7e0eca7b0aa
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-10-14 19:53:12 -0300

    testing fetch function

--- js/main.js
+++ js/main.js
@@ -3,16 +3,23 @@ const input = document.querySelector('#search');
 const titles = document.querySelectorAll('.title');
 const covers = document.querySelectorAll('.cover');
 const descriptions = document.querySelectorAll('.description');
-const status = document.querySelectorAll('.status');
+const scores = document.querySelectorAll('.score');
 
 const url =
   'https://api.themoviedb.org/3/movie/76341?api_key=aa86d7264e7595e6dd88b54da2d8fa9e';
-/* https://api.themoviedb.org/3/movie/550?api_key=aa86d7264e7595e6dd88b54da2d8fa9e
- */
 
-async function getData() {
-  const response = await axios.get(url);
+/* BASE URL = https://api.themoviedb.org/3/search/movie?api_key=aa86d7264e7595e6dd88b54da2d8fa9e&query=  <-- aqui vem o texto digitado */
+
+async function handleData() {
+  const inputValue = input.value;
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?api_key=aa86d7264e7595e6dd88b54da2d8fa9e&query=${inputValue}`
+  );
   console.log(response);
+  titles[0].innerText = response.data.results[0].original_title;
+  covers[0].src = `https://image.tmdb.org/t/p/w200${response.data.results[0].poster_path}`;
+  descriptions[0].innerText = response.data.results[0].overview;
+  scores[0].innerText = '★' + response.data.results[0].vote_average;
 }
 
-/* getData(); */
+btn.addEventListener('click', handleData);
