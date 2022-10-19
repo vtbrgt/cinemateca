e35a81b8858e58aa3577a7e9aa4e54486595898a
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-10-19 19:20:37 -0300

    shortening response object

--- js/main.js
+++ js/main.js
@@ -15,11 +15,12 @@ async function handleData() {
   const response = await axios.get(
     `https://api.themoviedb.org/3/search/movie?api_key=aa86d7264e7595e6dd88b54da2d8fa9e&query=${inputValue}`
   );
-  console.log(response);
-  titles[0].innerText = response.data.results[0].original_title;
-  covers[0].src = `https://image.tmdb.org/t/p/w200${response.data.results[0].poster_path}`;
-  descriptions[0].innerText = response.data.results[0].overview;
-  scores[0].innerText = '★' + response.data.results[0].vote_average;
+  const data = response.data.results;
+  console.log(data);
+  titles[0].innerText = data[0].original_title;
+  covers[0].src = `https://image.tmdb.org/t/p/w200${data[0].poster_path}`;
+  descriptions[0].innerText = data[0].overview;
+  scores[0].innerText = '★' + data[0].vote_average;
 }
 
 btn.addEventListener('click', handleData);
